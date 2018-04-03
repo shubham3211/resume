98f5f2f30dd583c4c9d332ed368e24cfc8237f53
shubham <kumarsinghshubham32111@gmail.com>
2018-04-03 13:11:54 +0530

    changes made

--- javascript/states/gameState/gameState.js
+++ javascript/states/gameState/gameState.js
@@ -5,8 +5,8 @@ resume.gameState = {
   init:function () {
 
    this.level=550;
-   this.brickWidth=90;
-   this.brickHeight=90;
+   this.brickWidth=100;
+   this.brickHeight=100;
 
   },
 
@@ -18,27 +18,43 @@ resume.gameState = {
     this.grass=this.add.tileSprite(0,this.level+40,this.game.width,100,'ground');
     this.grass=this.add.tileSprite(0,this.level-5,this.game.width,50,'grass');
 
+    this.applyTree();
     this.brickHeading();
+
   },
 
   brickHeading : function () {
 
-      this.bricks=this.add.group();
+    this.bricks=this.add.group();
+
     for(let i=0;i<6;i++){
 
-     var brick;
-        brick = this.game.add.sprite(this.game,this.game.world.centerX+this.brickWidth*i,this.level-80);
-        this.bricks.add(brick);
-        brick = this.game.add.sprite(this.game,this.game.world.centerX-this.brickWidth*i,this.level-80);
-        this.bricks.add(brick);
-    }
-
-
-    for(let i=0;i<4;i++){
-
-
+      if(i<=3){
+        this.bricks.create(this.game.world.centerX-i*this.brickWidth,this.level-90,'brick');
+        this.bricks.create(this.game.world.centerX+i*this.brickWidth,this.level-90,'brick');
+        this.bricks.create(this.game.world.centerX+i*this.brickWidth,this.level-190,'brick');
+        this.bricks.create(this.game.world.centerX-i*this.brickWidth,this.level-190,'brick');
+      }else{
+        this.bricks.create(this.game.world.centerX-i*this.brickWidth,this.level-90,'brick');
+        this.bricks.create(this.game.world.centerX+i*this.brickWidth,this.level-90,'brick');
+      }
 
     }
+
+    this.bricks.setAll('anchor.x',0.5);
+    this.bricks.setAll('width',this.brickWidth);
+    this.bricks.setAll('height',this.brickHeight);
+
+  },
+
+  applyTree:function () {
+    
+    this.game.add.sprite(120,100,'tree-bright-e')
+    this.game.add.sprite(10,310,'tree-dark-c')
+    this.game.add.sprite(900,225,'tree-dark-c')
+    this.game.add.sprite(1010,250,'tree-bright-d')
+    this.game.add.sprite(1150,450,'tree-dark-a')
+
 
   }
 };
--- javascript/states/preloadState/preloadState.js
+++ javascript/states/preloadState/preloadState.js
@@ -12,9 +12,17 @@ resume.prelaodState={
 
   preload:function () {
 
-    this.load.image('grass','assets/images/grass.png')
-    this.load.image('ground','assets/images/ground.png')
-    this.load.image('brick','assets/images/brick.png')
+    this.load.image('grass','assets/images/grass.png');
+    this.load.image('ground','assets/images/ground.png');
+    this.load.image('brick','assets/images/brick.png');
+    this.load.image('tree-bright-a','assets/images/tree/tree-bright-a.png');
+    this.load.image('tree-bright-b','assets/images/tree/tree-bright-b.png');
+    this.load.image('tree-bright-d','assets/images/tree/tree-bright-d.png');
+    this.load.image('tree-bright-e','assets/images/tree/tree-bright-e.png');
+    this.load.image('tree-dark-a','assets/images/tree/tree-dark-a.png');
+    this.load.image('tree-dark-b','assets/images/tree/tree-dark-b.png');
+    this.load.image('tree-dark-c','assets/images/tree/tree-dark-c.png');
+    this.load.image('tree-dark-d','assets/images/tree/tree-dark-d.png');
 
   },
 
